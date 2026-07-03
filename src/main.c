1a71374015445e3c9b928229d932ad14b6d8001c
Wesley Chiam <wc1425@ic.ac.uk>
2026-07-03 09:41:57 +0100

    Add input parser second pass skeleton, exit functionality

--- src/main.c
+++ src/main.c
@@ -1,14 +1,19 @@
-#include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
 
 #define INPUT_BUFFER 64
 #define MAX_ARGS 16
 
+typedef enum {
+    PARSE_OK,
+    PARSE_EXIT,
+    PARSE_FAIL
+} ParseResult;
+
 // Takes input string, representing prompted user input
 // Decomposes string into corresponding instruction(s)
 // Delegates to respective processes or handles errors 
-static void parse(char *inp) {
+static ParseResult parse(char *inp) {
     // First pass: split input string into tokens
     char *delims = " \t\n";
     char *ptr;
@@ -17,22 +22,34 @@ static void parse(char *inp) {
     int nargs = 0;
     while(token != NULL && nargs < MAX_ARGS - 1) {
         args[nargs] = token;
-        printf("%s ", args[nargs]);
         nargs++;
         token = strtok_r(NULL, delims, &ptr);
     }
     args[nargs] = NULL;
 
     // Second pass: compare tokens with defined functions
-    // TODO
+    if (strcmp(args[0], "ls") == 0) {
+        // Implement ls command
+        // TODO
+    } else if (strcmp(args[0], "pwd") == 0) {
+        // Implement pwd command
+        // TODO
+    } else if (strcmp(args[0], "exit") == 0) {
+        return PARSE_EXIT;
+    } else {
+        printf("%s: command not found\n", args[0]);
+        return PARSE_FAIL;
+    }
 }
 
 int main(void) {
-    bool running = true;
-    while (running) {
+    ParseResult status = PARSE_OK;
+    while (status != PARSE_EXIT) {
         char buffer[INPUT_BUFFER];
         printf("wush> ");
         fgets(buffer, sizeof(buffer), stdin);
-        parse(buffer);
+
+        status = parse(buffer);
+        
     }
 }
